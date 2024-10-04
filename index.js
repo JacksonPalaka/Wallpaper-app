3386ce8854411b840a899f2515460a31aebee517
Jackson palaka <jacksoncoco29@gmail.com>
2024-10-04 18:15:59 +0530

    changed colours

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
-const buttons= document.querySelectorAll('.button')
+const buttons = document.querySelectorAll('.button')
 
-buttons.forEach(function(button){
+buttons.forEach(function (button) {
     console.log(button)
-    button.addEventListener('click',function(e){
-            document.body.style.backgroundColor=e.target.id}
-        // document.body.style.backgroundColor(e.target.id)}
-)})
+    button.addEventListener('click', function (e) {
+        console.log(e.target.id)
+        if(e.target.id==='#212121'){
+            document.body.style.backgroundColor = e.target.id;
+            document.body.style.color='white';
+            
+        }else{
+            document.body.style.backgroundColor=e.target.id;
+            document.body.style.color='black';
 
+        }
+
+    })
+})
 
